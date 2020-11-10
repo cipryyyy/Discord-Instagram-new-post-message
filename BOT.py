2fb6386cce9b36461a5232163c8b439499c10ba7
Andrea Cipriani <63643557+cipryyyy@users.noreply.github.com>
2020-11-10 10:05:31 +0100

    Code fixes

--- BOT.py
+++ BOT.py
@@ -1,4 +1,3 @@
-  GNU nano 4.8                                                                                                      DeBug.py
 import discord
 from discord.ext import tasks
 from selenium.webdriver.firefox.options import Options
@@ -16,13 +15,13 @@ import sys
 client=discord.Client()
 account=#############
 
-pth="path"
+pth="path"                          #Main folder
 
 BCP=pth+"bcp.txt"
 IDF=pth+"channel_id.txt"
 BLG=pth+"bots_log.txt"
 
-if os.path.isfile(BCP)==True:
+if os.path.isfile(BCP)==True:                   #Check if files exist
     src=open(BCP,"r")
     last_link=src.read()
     src.close()
@@ -42,13 +41,12 @@ else:
     print("No log file, quitting")
     quit()
 
-@tasks.loop(minutes=1)
+@tasks.loop(minutes=1) 
 async def main(id_channel=id_channel):
-
-    global last_link
+    global last_link                            #link of the last post
     global pth
 
-    def DataGen(source):
+    def DataGen(source):                        #Generate name
         AW=None
         author=""
 
@@ -61,39 +59,39 @@ async def main(id_channel=id_channel):
             if AW==True:
                 author+=word+" "
 
-        author=author.replace("by ","")
+        author=author.replace("by ","")                 #"by user"--->"user"
         return author
 
     def LinkGen(source):
         href=source.split()[1]
-        partial_link=href[6:-1]
+        partial_link=href[6:-1]                         #partial link = /p/1a2bc3d4e5f
         return partial_link
 
     global account
-    page_link="https://www.instagram.com/{}/".format(account.replace("@",""))
+    page_link="https://www.instagram.com/{}/".format(account.replace("@",""))       #link of the account
     options=Options()
     options.add_argument("--headless")
     browser=webdriver.Firefox(options=options)
     browser.get(page_link)
 
-    code=browser.find_element_by_class_name("v1Nh3.kIKUG._bz0w").get_attribute('outerHTML')
-    tags=code.replace("><",">(SEP)<").split(sep="(SEP)")
+    code=browser.find_element_by_class_name("v1Nh3.kIKUG._bz0w").get_attribute('outerHTML')         #class of the last post
+    tags=code.replace("><",">(SEP)<").split(sep="(SEP)")                                            #splits tag
     for tag in tags:
-        if tag.startswith("<img"):
+        if tag.startswith("<img"):                                                          #in the img tag you can find username
             author=DataGen(tag)
         if tag.startswith("<a"):
-            link="https://www.instagram.com"+LinkGen(tag)
+            link="https://www.instagram.com"+LinkGen(tag)                   #full link = https://www.instagram.com/p/1a2bc3d4e5f
     browser.get(link)
     caption_code=browser.find_element_by_class_name("C4VMK").get_attribute('outerHTML')
-    caption_blocks=(caption_code.replace("<br>","\n")).replace("><",">(SEP)<").split(sep="(SEP)")
+    caption_blocks=(caption_code.replace("<br>","\n")).replace("><",">(SEP)<").split(sep="(SEP)")   #<br> in HTML == \n in python
     for tag in caption_blocks:
         if tag.startswith("<span"):
-            caption=(tag.split(sep="<")[1].split(sep=">")[1])
+            caption=(tag.split(sep="<")[1].split(sep=">")[1])                           #keeps inly the caption
     browser.close()
 
     captions_text=caption.split("\n")
     caption=captions_text[0]
-    if len(captions_text)>20:
+    if len(captions_text)>20:                                                           #shows noly the firsts twenty words
         caption=" ".join(captions_text[:20])+"..."
 
     if link!=last_link:
@@ -101,7 +99,7 @@ async def main(id_channel=id_channel):
         BL.write(news)
         print(colored(f"[{datetime.datetime.now()}]###!NEW POST!###","green"))
         last_link=link
-        backup=open(BCP,"w")
+        backup=open(BCP,"w")                                        #backup of the last post
         backup.write(last_link)
         backup.close()
         options=Options()
@@ -118,7 +116,7 @@ async def main(id_channel=id_channel):
             f.write(r.content)
         driver.close()
 
-        comm=f"@everyone NUOVO POST\n{author}\n{caption}\n\nLINK:\n{link}"
+        comm=f"@everyone NUOVO POST\n{author}\n{caption}\n\nLINK:\n{link}"          #message to send
         await channel.send(comm, file=discord.File(pth+name))
 
 @client.event
@@ -130,7 +128,7 @@ async def on_ready():
     try:
         main.start()
     except Exception:
-        exc_info=sys.exc_info()
+        exc_info=sys.exc_info()                                                     #Logs error
         err=traceback.format_exception(*exc_info)
         error_log="["+str(datetime.datetime.now())+"] "+str(err[-1])
         BL.write(error_log)

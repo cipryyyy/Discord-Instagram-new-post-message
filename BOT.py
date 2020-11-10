b26f49fbe09c67735cd576e40a59b41885acb063
cipryyyy <andrea254cipriani@gmail.com>
2020-11-10 09:51:54 +0100

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,3 +1,4 @@
+  GNU nano 4.8                                                                                                      DeBug.py
 import discord
 from discord.ext import tasks
 from selenium.webdriver.firefox.options import Options
@@ -13,54 +14,87 @@ import traceback
 import sys
 
 client=discord.Client()
+account=#############
 
-BCP=backup_file
-ID=id_of_the_channel
-LOG=log_file
-XPATH=full_xpath_of_last_post_from_user_page
+pth="path"
+
+BCP=pth+"bcp.txt"
+IDF=pth+"channel_id.txt"
+BLG=pth+"bots_log.txt"
 
 if os.path.isfile(BCP)==True:
     src=open(BCP,"r")
     last_link=src.read()
     src.close()
 else:
-    print("No backup file found, quitting")
+    print("No backup file, quitting")
     quit()
-if os.path.isfile(ID)==True:
-    id_src=open(ID,"r")
+if os.path.isfile(IDF)==True:
+    id_src=open(IDF,"r")
     id_channel=int(id_src.read())
     id_src.close()
 else:
-    print("No ID found, quitting")
+    print("No ID file, quitting")
     quit()
-
-if os.path.isfile(LOG)==True:
-    BL=open(LOG,"a")
+if os.path.isfile(BLG)==True:
+    BL=open(BLG,"a")
 else:
-    print("No log file found, quitting")
+    print("No log file, quitting")
     quit()
 
 @tasks.loop(minutes=1)
 async def main(id_channel=id_channel):
-    def last_post(profile):
-        LINK=profile
-        options = Options()
-        options.headless = True
-        driver = webdriver.Firefox(options=options)
-        driver.set_page_load_timeout(180)
-        driver.get(LINK)
-        code=driver.find_element_by_xpath(XPATH).get_attribute('outerHTML')
-        blocks=code.split(sep="<")
-        link="https://www.instagram.com"+blocks[2].split()[1][6:-1]
-        driver.close()
-        return link
 
-    channel=client.get_channel(id_channel)
     global last_link
-    START=time.time()
-    account=#############
+    global pth
 
-    link=last_post(f"https://www.instagram.com/{account}/")
+    def DataGen(source):
+        AW=None
+        author=""
+
+        words=source.split()
+        for word in words:
+            if word=="by":
+                AW=True
+            if word=="in" or word=="on":
+                AW=False
+            if AW==True:
+                author+=word+" "
+
+        author=author.replace("by ","")
+        return author
+
+    def LinkGen(source):
+        href=source.split()[1]
+        partial_link=href[6:-1]
+        return partial_link
+
+    global account
+    page_link="https://www.instagram.com/{}/".format(account.replace("@",""))
+    options=Options()
+    options.add_argument("--headless")
+    browser=webdriver.Firefox(options=options)
+    browser.get(page_link)
+
+    code=browser.find_element_by_class_name("v1Nh3.kIKUG._bz0w").get_attribute('outerHTML')
+    tags=code.replace("><",">(SEP)<").split(sep="(SEP)")
+    for tag in tags:
+        if tag.startswith("<img"):
+            author=DataGen(tag)
+        if tag.startswith("<a"):
+            link="https://www.instagram.com"+LinkGen(tag)
+    browser.get(link)
+    caption_code=browser.find_element_by_class_name("C4VMK").get_attribute('outerHTML')
+    caption_blocks=(caption_code.replace("<br>","\n")).replace("><",">(SEP)<").split(sep="(SEP)")
+    for tag in caption_blocks:
+        if tag.startswith("<span"):
+            caption=(tag.split(sep="<")[1].split(sep=">")[1])
+    browser.close()
+
+    captions_text=caption.split("\n")
+    caption=captions_text[0]
+    if len(captions_text)>20:
+        caption=" ".join(captions_text[:20])+"..."
 
     if link!=last_link:
         news="["+str(datetime.datetime.now())+"] "+"New post: "+str(link)+"\n"
@@ -80,14 +114,18 @@ async def main(id_channel=id_channel):
         img_url = img['src']
         r = requests.get(img_url)
         name="file.png"
-        with open("/path/"+name,'wb') as f:
+        with open(pth+name,'wb') as f:
             f.write(r.content)
         driver.close()
-        await channel.send(f"@everyone Ultimo post di {account}\n{link}", file=discord.File("/path/"+name))
+
+        comm=f"@everyone NUOVO POST\n{author}\n{caption}\n\nLINK:\n{link}"
+        await channel.send(comm, file=discord.File(pth+name))
+
 @client.event
 async def on_ready():
-    BL.write(str(datetime.datetime.now()))
-    BL.write(" Started\n")
+    global id_channel
+    start_log="["+str(datetime.datetime.now())+"] "+"BOT ONLINE ("+str(id_channel)+")\n"
+    BL.write(start_log)
     print(colored(f"Logged in as {client.user.name} [{client.user.id}]","cyan"))
     try:
         main.start()
@@ -98,6 +136,6 @@ async def on_ready():
         BL.write(error_log)
         BL.close()
         quit()
-    return await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=))
+    return await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=account))
 
 client.run(TOKEN)
